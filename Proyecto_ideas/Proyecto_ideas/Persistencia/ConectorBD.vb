61e7b6067e72ece8627027733b3fee6efd1e3890
unknown <SenorStab.brianelmaster10@gmail.com>
2019-09-12 12:26:05 -0300

    Se creo la coneccion con la base de datos

--- Proyecto_ideas/Proyecto_ideas/Persistencia/ConectorBD.vb
+++ Proyecto_ideas/Proyecto_ideas/Persistencia/ConectorBD.vb
@@ -1,3 +1,19 @@
-﻿Public Class ConectorBD
+﻿Imports MySql.Data.MySqlClient
+Public Class ConectorBD
+    Private stringConexion As String
+    Private objConexion As MySqlConnection
 
+    Public Sub New()
+        Me.stringConexion = "server=localhost;Database=ideas;uid=root;pwd= "
+        Me.objConexion = New MySqlConnection(Me.stringConexion)
+    End Sub
+
+    Public Sub Conectar()
+        Try
+            Me.objConexion.Open()
+            'Borrar siguiente línea
+            MsgBox("Conexión exitosa.")
+        Catch ex As Exception
+            MsgBox("Conexión falló.")
+        End Try
 End Class
