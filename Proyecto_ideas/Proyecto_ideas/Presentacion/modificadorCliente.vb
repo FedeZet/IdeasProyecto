e211ad3ea4a16e3b2b31ab2881e9e38cd8ab6e54
WorldChallenge <RodrigoCarbone01@gmail.com>
2019-10-17 18:22:30 -0300

    modificador cliente

---
+++ Proyecto_ideas/Proyecto_ideas/Presentacion/modificadorCliente.vb
@@ -0,0 +1,25 @@
+﻿Public Class modificadorCliente
+
+    Dim idCliente
+
+    Private Sub btnModificarC_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnModificarC.Click
+        Dim objManCliente As ManCliente = New ManCliente
+        objManCliente.modificarCliente(idCliente, txtMCnombre.Text, txtMCtelefono.Text)
+    End Sub
+
+    Private Sub btnCancelarC_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancelarC.Click
+        Lista_clientesvb.Show()
+        Me.Close()
+    End Sub
+
+    Public Function cargarRepuesto(ByVal id As Integer, ByVal nombre As String, ByVal telefono As String)
+        idCliente = id
+        txtMCnombre.Text = nombre
+        txtMCtelefono.Text = telefono
+        Return True
+    End Function
+
+    Private Sub modificadorCliente_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
