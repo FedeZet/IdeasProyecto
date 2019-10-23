21ebe3429eef8d0fb365bac33ebe60372f1bf047
WorldChallenge <RodrigoCarbone01@gmail.com>
2019-10-23 20:07:55 -0300

    Se agrego el codigo de lista

--- Proyecto_ideas/Proyecto_ideas/Presentacion/Historial_ordenes.Designer.vb
+++ Proyecto_ideas/Proyecto_ideas/Presentacion/Historial_ordenes.Designer.vb
@@ -22,10 +22,7 @@ Partial Class Historial_ordenes
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.btnVolverOrdenesLista = New System.Windows.Forms.Button()
-        Me.btnAgregarO = New System.Windows.Forms.Button()
-        Me.btnListaImprimir = New System.Windows.Forms.Button()
+        Me.dvgListaOrdenes = New System.Windows.Forms.DataGridView()
         Me.idO = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.detalle = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.resolucion = New System.Windows.Forms.DataGridViewTextBoxColumn()
@@ -38,48 +35,27 @@ Partial Class Historial_ordenes
         Me.idU = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.idC = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.idD = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.dgvListaOrdenes = New System.Windows.Forms.DataGridView()
+        Me.btnCModificar = New System.Windows.Forms.Button()
+        Me.btnCEliminar = New System.Windows.Forms.Button()
+        Me.btnListaImprimir = New System.Windows.Forms.Button()
+        Me.btnAgregarO = New System.Windows.Forms.Button()
+        Me.btnVolverOrdenesLista = New System.Windows.Forms.Button()
+        CType(Me.dvgListaOrdenes, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvListaOrdenes, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'DataGridView1
+        'dvgListaOrdenes
         '
-        Me.DataGridView1.AllowUserToAddRows = False
-        Me.DataGridView1.AllowUserToDeleteRows = False
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.idO, Me.detalle, Me.resolucion, Me.importe, Me.dolar, Me.garantia, Me.fecha, Me.hora, Me.estado, Me.idU, Me.idC, Me.idD})
-        Me.DataGridView1.Location = New System.Drawing.Point(32, 12)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.ReadOnly = True
-        Me.DataGridView1.Size = New System.Drawing.Size(1242, 210)
-        Me.DataGridView1.TabIndex = 0
-        '
-        'btnVolverOrdenesLista
-        '
-        Me.btnVolverOrdenesLista.Location = New System.Drawing.Point(517, 269)
-        Me.btnVolverOrdenesLista.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
-        Me.btnVolverOrdenesLista.Name = "btnVolverOrdenesLista"
-        Me.btnVolverOrdenesLista.Size = New System.Drawing.Size(75, 23)
-        Me.btnVolverOrdenesLista.TabIndex = 1
-        Me.btnVolverOrdenesLista.Text = "Volver"
-        Me.btnVolverOrdenesLista.UseVisualStyleBackColor = True
-        '
-        'btnAgregarO
-        '
-        Me.btnAgregarO.Location = New System.Drawing.Point(616, 268)
-        Me.btnAgregarO.Name = "btnAgregarO"
-        Me.btnAgregarO.Size = New System.Drawing.Size(75, 23)
-        Me.btnAgregarO.TabIndex = 2
-        Me.btnAgregarO.Text = "Agregar"
-        Me.btnAgregarO.UseVisualStyleBackColor = True
-        '
-        'btnListaImprimir
-        '
-        Me.btnListaImprimir.Location = New System.Drawing.Point(715, 268)
-        Me.btnListaImprimir.Name = "btnListaImprimir"
-        Me.btnListaImprimir.Size = New System.Drawing.Size(75, 23)
-        Me.btnListaImprimir.TabIndex = 3
-        Me.btnListaImprimir.Text = "Imprimir"
-        Me.btnListaImprimir.UseVisualStyleBackColor = True
+        Me.dvgListaOrdenes.AllowUserToAddRows = False
+        Me.dvgListaOrdenes.AllowUserToDeleteRows = False
+        Me.dvgListaOrdenes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dvgListaOrdenes.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.idO, Me.detalle, Me.resolucion, Me.importe, Me.dolar, Me.garantia, Me.fecha, Me.hora, Me.estado, Me.idU, Me.idC, Me.idD})
+        Me.dvgListaOrdenes.Location = New System.Drawing.Point(32, 12)
+        Me.dvgListaOrdenes.Name = "dvgListaOrdenes"
+        Me.dvgListaOrdenes.ReadOnly = True
+        Me.dvgListaOrdenes.Size = New System.Drawing.Size(1242, 210)
+        Me.dvgListaOrdenes.TabIndex = 0
         '
         'idO
         '
@@ -153,26 +129,85 @@ Partial Class Historial_ordenes
         Me.idD.Name = "idD"
         Me.idD.ReadOnly = True
         '
+        'dgvListaOrdenes
+        '
+        Me.dgvListaOrdenes.AllowUserToAddRows = False
+        Me.dgvListaOrdenes.AllowUserToDeleteRows = False
+        Me.dgvListaOrdenes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvListaOrdenes.Location = New System.Drawing.Point(32, 12)
+        Me.dgvListaOrdenes.Name = "dgvListaOrdenes"
+        Me.dgvListaOrdenes.ReadOnly = True
+        Me.dgvListaOrdenes.Size = New System.Drawing.Size(1242, 210)
+        Me.dgvListaOrdenes.TabIndex = 0
+        '
+        'btnCModificar
+        '
+        Me.btnCModificar.Enabled = False
+        Me.btnCModificar.Location = New System.Drawing.Point(611, 256)
+        Me.btnCModificar.Name = "btnCModificar"
+        Me.btnCModificar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCModificar.TabIndex = 45
+        Me.btnCModificar.Text = "Modificar"
+        Me.btnCModificar.UseVisualStyleBackColor = True
+        '
+        'btnCEliminar
+        '
+        Me.btnCEliminar.Enabled = False
+        Me.btnCEliminar.Location = New System.Drawing.Point(802, 256)
+        Me.btnCEliminar.Name = "btnCEliminar"
+        Me.btnCEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCEliminar.TabIndex = 44
+        Me.btnCEliminar.Text = "Eliminar"
+        Me.btnCEliminar.UseVisualStyleBackColor = True
+        '
+        'btnListaImprimir
+        '
+        Me.btnListaImprimir.Location = New System.Drawing.Point(701, 256)
+        Me.btnListaImprimir.Name = "btnListaImprimir"
+        Me.btnListaImprimir.Size = New System.Drawing.Size(75, 23)
+        Me.btnListaImprimir.TabIndex = 43
+        Me.btnListaImprimir.Text = "Imprimir"
+        Me.btnListaImprimir.UseVisualStyleBackColor = True
+        '
+        'btnAgregarO
+        '
+        Me.btnAgregarO.Location = New System.Drawing.Point(514, 256)
+        Me.btnAgregarO.Name = "btnAgregarO"
+        Me.btnAgregarO.Size = New System.Drawing.Size(75, 23)
+        Me.btnAgregarO.TabIndex = 42
+        Me.btnAgregarO.Text = "Agregar"
+        Me.btnAgregarO.UseVisualStyleBackColor = True
+        '
+        'btnVolverOrdenesLista
+        '
+        Me.btnVolverOrdenesLista.Location = New System.Drawing.Point(415, 257)
+        Me.btnVolverOrdenesLista.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnVolverOrdenesLista.Name = "btnVolverOrdenesLista"
+        Me.btnVolverOrdenesLista.Size = New System.Drawing.Size(75, 23)
+        Me.btnVolverOrdenesLista.TabIndex = 41
+        Me.btnVolverOrdenesLista.Text = "Volver"
+        Me.btnVolverOrdenesLista.UseVisualStyleBackColor = True
+        '
         'Historial_ordenes
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1298, 303)
+        Me.Controls.Add(Me.btnCModificar)
+        Me.Controls.Add(Me.btnCEliminar)
         Me.Controls.Add(Me.btnListaImprimir)
         Me.Controls.Add(Me.btnAgregarO)
         Me.Controls.Add(Me.btnVolverOrdenesLista)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.dvgListaOrdenes)
         Me.Name = "Historial_ordenes"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Historial_ordenes"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dvgListaOrdenes, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvListaOrdenes, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
-    Friend WithEvents btnVolverOrdenesLista As System.Windows.Forms.Button
-    Friend WithEvents btnAgregarO As System.Windows.Forms.Button
-    Friend WithEvents btnListaImprimir As System.Windows.Forms.Button
+    Friend WithEvents dvgListaOrdenes As System.Windows.Forms.DataGridView
     Friend WithEvents idO As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents detalle As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents resolucion As System.Windows.Forms.DataGridViewTextBoxColumn
@@ -185,4 +220,10 @@ Partial Class Historial_ordenes
     Friend WithEvents idU As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents idC As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents idD As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents dgvListaOrdenes As System.Windows.Forms.DataGridView
+    Friend WithEvents btnCModificar As System.Windows.Forms.Button
+    Friend WithEvents btnCEliminar As System.Windows.Forms.Button
+    Friend WithEvents btnListaImprimir As System.Windows.Forms.Button
+    Friend WithEvents btnAgregarO As System.Windows.Forms.Button
+    Friend WithEvents btnVolverOrdenesLista As System.Windows.Forms.Button
 End Class
--- Proyecto_ideas/Proyecto_ideas/Presentacion/Historial_ordenes.vb
+++ Proyecto_ideas/Proyecto_ideas/Presentacion/Historial_ordenes.vb
@@ -1,16 +1,80 @@
 ﻿Public Class Historial_ordenes
 
-    Private Sub btnVolverOrdenesLista_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnVolverOrdenesLista.Click
+    Public Sub New()
+        InitializeComponent()
+        Dim objManOrden As ManOrden = New ManOrden
+        Dim hashOrden As Hashtable
+        hashOrden = objManOrden.obtenerOrden()
+        Me.CargarDGV(hashOrden)
+
+    End Sub
+
+    Sub CargarDGV(ByVal colOrden As Hashtable)
+        For Each DEntry As DictionaryEntry In colOrden
+            Dim objO As Orden = CType(DEntry.Value, Orden)
+            Me.dgvListaOrdenes.Rows.Add()
+            Dim dolar, garantia As String
+            If objO.Dolar = True Then
+                dolar = "Si"
+            Else
+                dolar = "No"
+            End If
+            If objO.Garantia = True Then
+                garantia = "Si"
+            Else
+                garantia = "No"
+            End If
+
+            Dim cantFilas As Integer = Me.dgvListaOrdenes.Rows.Count - 1
+            Me.dgvListaOrdenes(0, cantFilas).Value = objO.IdOrden.ToString
+            Me.dgvListaOrdenes(1, cantFilas).Value = objO.Falla
+            Me.dgvListaOrdenes(2, cantFilas).Value = objO.Resolucion
+            Me.dgvListaOrdenes(3, cantFilas).Value = objO.Importe.ToString
+            Me.dgvListaOrdenes(4, cantFilas).Value = dolar
+            Me.dgvListaOrdenes(5, cantFilas).Value = garantia
+            Me.dgvListaOrdenes(6, cantFilas).Value = objO.Fecha
+            Me.dgvListaOrdenes(7, cantFilas).Value = objO.Hora
+            Me.dgvListaOrdenes(8, cantFilas).Value = objO.Estado
+            Me.dgvListaOrdenes(9, cantFilas).Value = objO.idU.ToString
+            Me.dgvListaOrdenes(10, cantFilas).Value = objO.idC.ToString
+            Me.dgvListaOrdenes(11, cantFilas).Value = objO.idD.ToString
+
+        Next
+    End Sub
+
+    Private Sub btnVolverOrdenesLista_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
         Menu_Inicio.Show()
         Me.Close()
     End Sub
 
-    Private Sub btnAgregarO_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAgregarO.Click
+    Private Sub btnAgregarO_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
         Registro_orden.Show()
     End Sub
 
-    Private Sub btnListaImprimir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnListaImprimir.Click
+    Private Sub btnListaImprimir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
         Me.Hide()
         ImprimirOrden.Show()
     End Sub
+
+    Private Sub btnCModificar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCModificar.Click
+
+    End Sub
+
+    Private Sub btnCEliminar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCEliminar.Click
+
+        Dim objManOrden As ManOrden = New ManOrden
+        Dim resultado As Integer = MsgBox("Desea Eliminar esta orden? ", vbYesNo + vbQuestion)
+        If resultado = vbYes Then
+            objManOrden.eliminarOrden(dgvListaOrdenes.CurrentRow.Cells("idO").Value.ToString)
+            MsgBox("La orden ha sido eliminada con éxito.")
+        Else
+            MsgBox("La orden no ha sido eliminada.")
+        End If
+
+    End Sub
+
+    Private Sub Historial_ordenes_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
 End Class
